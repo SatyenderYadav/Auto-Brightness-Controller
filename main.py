0db32ce397a568d40f52c380f48c5c542fa5f702
Satyender Yadav <54953623+SatyenderYadav@users.noreply.github.com>
2020-05-28 18:25:51 +0530

    main.py

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+import psutil
 import wmi
 
-brightness = 100 # percentage [0-100]
-c = wmi.WMI(namespace='wmi')
+#brightness control function 
+def brightness(value):
+    brightness = value # percentage [0-100]
+    c = wmi.WMI(namespace='wmi')
+    methods = c.WmiMonitorBrightnessMethods()[0]
+    methods.WmiSetBrightness(brightness, 0)
+    
+#Give Battery percentage     
+battery = psutil.sensors_battery()
+percent = (battery.percent)
 
-methods = c.WmiMonitorBrightnessMethods()[0]
-methods.WmiSetBrightness(brightness, 0)
+if percent == 100:
+    brightness(100)
+elif percent < 100 and percent >= 90 :
+    brightness(90)
+elif percent < 90 and percent >= 80 :
+    brightness(80)
+elif percent < 80 and percent >= 60 :
+    brightness(60)
+elif percent < 60 and percent >= 0 :
+    brightness(40)
 
- 
+     
